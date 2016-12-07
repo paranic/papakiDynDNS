750cf86b1de1cfae7b377413b61bcb70cbabca98
qscwdzeax <github@robros.se>
2016-12-07 22:53:05 +0100

    Bug fixes
    Trim the response from the IP gathering site and encode the password string in case it contains ampersand (&)

--- papakiDynDNS.php
+++ papakiDynDNS.php
@@ -15,9 +15,9 @@ include('dom/simple_html_dom.php');
 
 $config['host'] = 'your_host_www';
 $config['domain'] = 'your_papaki_domain';
-$config['new_ip_address'] = file_get_contents('http://icanhazip.com/');
+$config['new_ip_address'] = trim(file_get_contents('http://icanhazip.com/'));
 $config['papaki_username'] = 'your_papaki_gr_username';
-$config['papaki_password'] = 'your_papaki_gr_password';
+$config['papaki_password'] = urlencode('your_papaki_gr_password');
 
 // Do the login
 $ch = curl_init();
